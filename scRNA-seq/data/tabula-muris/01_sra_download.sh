c40841368d50d3225e0087cddbc0f988cf4a5ac8
isturgill <51013120+isturgill@users.noreply.github.com>
2026-08-13 12:50:14 -0400

    Removed --dryrun; BAM to variable

--- scRNA-seq/data/tabula-muris/01_sra_download.sh
+++ scRNA-seq/data/tabula-muris/01_sra_download.sh
@@ -3,8 +3,9 @@
 
 S3_BUCKET="sra-pub-src-1"
 SRR="SRR6835847"
+BAM="10X_P4_3.bam.1"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -euo pipefail
 
-aws s3 cp s3://${S3_BUCKET}/${SRR}/10X_P4_3.bam.1 . --no-sign-request --dryrun
+aws s3 cp s3://${S3_BUCKET}/${SRR}/${BAM} . --no-sign-request
